8e6e3b841df7050a7d2ead33fc8780e2da65c1d2
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-09-18 14:25:46 +0200

    Increment version of ethereum:web3 to 0.20.2

--- frontend/packages/dapple/package.js
+++ frontend/packages/dapple/package.js
@@ -7,10 +7,8 @@ Package.describe({
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.4.0.1');
-
   api.use('ecmascript', 'client');
-  api.use('ethereum:web3', 'client');
+  api.use('ethereum:web3@0.20.2', 'client');
 
   api.addFiles(['package-pre-init.js'], 'client');
   api.addFiles(['contracts-abi/maker-otc.js'], 'client');
